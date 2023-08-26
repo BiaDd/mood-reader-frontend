a14001ddc52e3c4dff4aaa9d6b021c67e69a63bc
Dan Do <dxd3vmb@virginia.edu>
2023-08-26 11:49:59 -0400

    fixing production app

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -26,15 +26,13 @@ const useStyles = makeStyles((theme) => ({
 const VideoPlayer = () => {
     const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
     const classes = useStyles();
-
+    console.log(myVideo);
     return (
         <Grid container className={classes.gridContainer}>
             {stream && (
                 <Paper className={classes.paper}>
                     <Grid item xs={12} md={6}>
-                        <Typography variant="h5" gutterBottom>
-                            {name || "Name"}
-                        </Typography>
+                        <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
                         <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
                     </Grid>
                 </Paper>
